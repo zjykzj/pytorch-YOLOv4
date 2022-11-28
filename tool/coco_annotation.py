55227e8ef1ef0d8bd66edb57eaf98fb56870580d
zjykzj <wy163zhuj@163.com>
2022-11-28 16:49:40 +0800

    chore(coco_annotations.py): update train.txt/val.txt use

--- tool/coco_annotation.py
+++ tool/coco_annotation.py
@@ -16,9 +16,15 @@ from tqdm import tqdm
 import os
 
 """hyper parameters"""
-json_file_path = 'E:/Dataset/mscoco2017/annotations/instances_train2017.json'
-images_dir_path = 'mscoco2017/train2017/'
-output_path = '../data/val.txt'
+# json_file_path = 'E:/Dataset/mscoco2017/annotations/instances_train2017.json'
+# images_dir_path = 'mscoco2017/train2017/'
+# output_path = '../data/val.txt'
+# json_file_path = './coco/annotations/instances_train2017.json'
+# images_dir_path = './coco/images/train2017/'
+# output_path = './data/train.txt'
+json_file_path = './coco/annotations/instances_val2017.json'
+images_dir_path = './coco/images/val2017/'
+output_path = './data/val.txt'
 
 """load json file"""
 name_box_id = defaultdict(list)
